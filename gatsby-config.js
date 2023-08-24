0d72463939aa39a3e9b55f1de06de589c0378673
marianaLz <marianaglp15@gmail.com>
2023-08-23 19:13:17 -0600

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Acabados Palma`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
